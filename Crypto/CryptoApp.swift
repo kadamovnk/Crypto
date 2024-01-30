a94410c9316032b89cc581d74013ccdecb2f01a7
Nodirbek Kadamov <kadamovnk@icloud.com>
2024-01-30 19:59:19 +0500

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Nodirbek Kadamov on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
